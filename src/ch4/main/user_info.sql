cf8380a044b9bdfb564719efc8157722b4bd8ca2
liukang07 <liukang07@baidu.com>
2021-11-18 16:34:25 +0800

    user_info.sql

---
+++ src/ch4/main/user_info.sql
@@ -0,0 +1,10 @@
+-- auto-generated definition
+create table user_info
+(
+    uid         int auto_increment
+        primary key,
+    user_name   varchar(128) null,
+    depart_name varchar(128) null,
+    created     varchar(128) null
+);
+
